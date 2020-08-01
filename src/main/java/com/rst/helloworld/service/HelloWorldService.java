1f6362b5cb7bca491fb907f01bfe31c1aabdb53e
jagadeeshreddybj <55692777+jagadeeshreddybj@users.noreply.github.com>
2020-08-01 09:17:05 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -17,6 +17,22 @@ public class HelloWorldService {
 		return "Maven + Spring MVC + Jenkins + Docker Example";
 
 	}
+	
+	 public static void main(String[] args) {
+ 
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+ 
+        } catch (UnknownHostException e) {
+ 
+            e.printStackTrace();
+        }
+    }
 
 	public String getTitle(String name) {
 
